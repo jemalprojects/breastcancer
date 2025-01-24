955827408e3b1f271fd2e18763a79e35f24c855a
jemalprojects <168229396+jemalprojects@users.noreply.github.com>
2025-01-24 19:15:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,57 +26,74 @@ hide_st_text = """
             """
 st.markdown(hide_st_text, unsafe_allow_html=True)
 
+with st.sidebar:
+    choose = option_menu("MENU", ["Home", "Make Prediction", "Retrain Model", "Update Datasets", "Learn"],
+                         icons=['house', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
+# logo = Image.open(r'C:\Users\13525\Desktop\Insights_Bees_logo.png')
+# profile = Image.open(r'C:\Users\13525\Desktop\medium_profile.png')
+if choose == "Home":
+    st.write("Home")
+elif choose == "Make Prediction":
+    st.subheader("Breast Cancer Detection", divider=True)
 
-model_dir = snapshot_download("abatejemal/breastcancer")
-
-# Parameters
-img_height = 224
-img_width = 224
-batch_size = 16
-
-# Streamlit app
-st.title("Breast Cancer Classification")
-
-# File uploader
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-
-# Submit button
-if uploaded_file:
-    # Display the uploaded image
-    # st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
-
-    # Add a button to process the image
-    if st.button("Submit for Classification"):
-        with st.spinner("Processing..."):
-            try:
-                # Load the image and resize it
-                image = load_img(uploaded_file, target_size=(img_height, img_width))
-                
-                # Convert the image to a numpy array and expand dimensions
-                image_array = img_to_array(image)
-                image_array = tf.expand_dims(image_array, axis=0)  # Add batch dimension
-                
-                # Normalize the image
-                image_array = image_array / 255.0  # Assuming model expects input in range [0, 1]
-                
-                # Load the saved model
-                # export_dir = 'MobileNetV2_save_model/'  # Adjust path if needed
-                # export_dir = '/MobileNetV2_save_model_finetuned/'  # Adjust path if needed
-                export_dir = f'{model_dir}/MobileNetV2_save_model_finetuned/'
-                loaded_model = tf.keras.models.load_model(export_dir)
-                
-                # Make predictions
-                predictions = loaded_model.predict(image_array)
-                predicted_class = np.argmax(predictions, axis=1)
-                
-                # Display predictions
-                st.success("Prediction completed!")
-                st.subheader("Prediction Result:")
-                result=""
-                if predicted_class[0]==1:
-                    result="Positive"
-                if predicted_class[0]==0:
-                    result="Negative"
-                st.write(f"Predicted Class: {result}")
-            except Exception as e:
-                st.error(f"Error: {e}")
+    model_dir = snapshot_download("abatejemal/breastcancer")
+    
+    # Parameters
+    img_height = 224
+    img_width = 224
+    batch_size = 16
+    
+    # Streamlit app
+    st.title("Breast Cancer Classification")
+    
+    # File uploader
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    
+    # Submit button
+    if uploaded_file:
+        # Display the uploaded image
+        # st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    
+        # Add a button to process the image
+        if st.button("Submit for Classification"):
+            with st.spinner("Processing..."):
+                try:
+                    # Load the image and resize it
+                    image = load_img(uploaded_file, target_size=(img_height, img_width))
+                    
+                    # Convert the image to a numpy array and expand dimensions
+                    image_array = img_to_array(image)
+                    image_array = tf.expand_dims(image_array, axis=0)  # Add batch dimension
+                    
+                    # Normalize the image
+                    image_array = image_array / 255.0  # Assuming model expects input in range [0, 1]
+                    
+                    # Load the saved model
+                    # export_dir = 'MobileNetV2_save_model/'  # Adjust path if needed
+                    # export_dir = '/MobileNetV2_save_model_finetuned/'  # Adjust path if needed
+                    export_dir = f'{model_dir}/MobileNetV2_save_model_finetuned/'
+                    loaded_model = tf.keras.models.load_model(export_dir)
+                    
+                    # Make predictions
+                    predictions = loaded_model.predict(image_array)
+                    predicted_class = np.argmax(predictions, axis=1)
+                    
+                    # Display predictions
+                    st.success("Prediction completed!")
+                    st.subheader("Prediction Result:")
+                    result=""
+                    if predicted_class[0]==1:
+                        result="Positive"
+                    if predicted_class[0]==0:
+                        result="Negative"
+                    st.write(f"Predicted Class: {result}")
+                except Exception as e:
+                    st.error(f"Error: {e}")
